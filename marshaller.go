9deed7e048abf4f14b2ef2427845723490ff3087
yra <ymz.ncnk@outlook.com>
2025-04-09 23:02:34 +0100

    Refactoring

--- marshaller.go
+++ marshaller.go
@@ -7,3 +7,10 @@ type MarshallerMUS interface {
 	MarshalMUS(w muss.Writer) (n int, err error)
 	SizeMUS() (size int)
 }
+
+// MarshallerTypedMUS interface wraps MarhsalMUS and SizeMUS methods. It is
+// intended for use with DTS.
+type MarshallerTypedMUS interface {
+	MarshalTypedMUS(w muss.Writer) (n int, err error)
+	SizeTypedMUS() (size int)
+}
